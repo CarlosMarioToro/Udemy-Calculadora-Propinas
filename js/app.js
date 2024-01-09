f76c80fbde3f6dc2f847ec54ad794ab6254728dd
CarlosMarioToro <111100510+CarlosMarioToro@users.noreply.github.com>
2024-01-08 19:08:03 -0500

    300-Eliminando Productos si la cantidad es 0

--- js/app.js
+++ js/app.js
@@ -131,7 +131,8 @@ function agregarPlatillo(producto) {
             cliente.pedido = [...pedido, producto];
         }
     } else {
-        console.log('No es mayor a 0');
+        // Eliminar elementos cuando la cantidad es 0
+        const resultado = pedido.filter(articulo => articulo.id !== producto.id);
+        cliente.pedido = [...resultado];
     }
-    console.log(cliente.pedido);
 }
